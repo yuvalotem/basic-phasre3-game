6364d66d05728dee4b5d1eb454d95320fe29cb1b
yuvalotem <yuvalotem@gmail.com>
2020-12-28 13:25:52 +0200

    basic game

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+
+    next()
+})
+
+const port = 3000
+app.listen(port, function(req, res){
+    console.log('running on ' + port);
+})
